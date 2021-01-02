8c9c3de7d15dcf94236d869763da7a6b1fe4d74f
Ravi Mendis <rmendis@mac.com>
2021-01-02 13:13:54 +0000

    Revised True Start Assignments
    Fix Singapore TSL for Standard Rules and R&F.

--- Scripts/AssignTrueStartingPlots.lua
+++ Scripts/AssignTrueStartingPlots.lua
@@ -2,6 +2,10 @@
 -- Author: blkbutterfly74
 -- DateCreated: 3/17/2018 10:55:30 AM
 ------------------------------------------------------------------------------
+include "MapEnums"
+include "MapUtilities"
+
+------------------------------------------------------------------------------
 AssignTrueStartingPlots = {};
 ------------------------------------------------------------------------------
 function AssignTrueStartingPlots.Create(args)
@@ -10,6 +14,8 @@ function AssignTrueStartingPlots.Create(args)
 		-- Core Process member methods
 		__InitStartingData				= AssignTrueStartingPlots.__InitStartingData,
 		__GetIndex						= AssignTrueStartingPlots.__GetIndex,
+		__TryToSetPlayerStartingPlot	= AssignTrueStartingPlots.__TryToSetPlayerStartingPlot,
+		__GetAdjacentPlots				= AssignTrueStartingPlots.__GetAdjacentPlots,
 
 		tsls = args.TRUE_START_LOCATIONS,
 		centerX = args.CENTER_X,
@@ -20,6 +26,7 @@ function AssignTrueStartingPlots.Create(args)
 
 	return instance;
 end
+
 ------------------------------------------------------------------------------
 function AssignTrueStartingPlots:__InitStartingData()
 	for _, pPlayer in ipairs(Players) do
@@ -37,38 +44,56 @@ function AssignTrueStartingPlots:__InitStartingData()
 					local i = self:__GetIndex(v[1], v[2]);
 					local pPlot = Map.GetPlotByIndex(i);
 
-					if (pPlayer == Game.GetLocalPlayer() and (pPlot:IsLake() or pPlot:IsMountain())) then
-						error("Can't place TSL for local player", 2);	-- must throw exception
-					else
-						if (pPlot:IsMountain()) then
-							error("Mountain block TSL for " .. leaderName .. " at index: " .. i); 
-						elseif (pPlot:IsLake()) then
-							error("Lake block TSL for " .. leaderName .. " at index: " .. i); 
-						else	
-							if (pPlot:IsWater()) then
-								print("WARNING: Water placement for", leaderName .. " at index: " .. i); 
-							end
-
-							-- o/w assign TSL
-							pPlayer:SetStartingPlot(pPlot);
-					
-							print("Assigning TSL for", leaderName .. " at index: " .. i); 
-						end
-					end
+					self:__TryToSetPlayerStartingPlot(pPlayer, leaderName, pPlot);
 				end
 			end
 		end
-
-		--[[
-		local pPlot = pPlayer:GetStartingPlot();
-		if (pPlot ~= nil) then
-			print("DEBUG: starting plot index: " .. pPlot:GetIndex());
-		else
-			--print("DEBUG: no starting plot assigned for " .. pPlayerConfig:GetLeaderTypeName());
-		end
-		--]]
 	end 
 end
+
+------------------------------------------------------------------------------
+function AssignTrueStartingPlots:__GetAdjacentPlots(pPlot)
+	local aPlots = {};
+
+	for direction = 0, DirectionTypes.NUM_DIRECTION_TYPES - 1, 1 do
+		local adjacentPlot = Map.GetAdjacentPlot(pPlot:GetX(), pPlot:GetY(), direction);
+		if (adjacentPlot ~= nil) then
+			table.insert(aPlots, adjacentPlot);
+		end
+	end
+
+	return aPlots;
+end
+
+------------------------------------------------------------------------------
+function AssignTrueStartingPlots:__TryToSetPlayerStartingPlot(pPlayer, leaderName, pPlot)
+	local i = pPlot:GetIndex();
+
+	if (pPlot:IsMountain()) then
+		print("NOTE: Mountain block TSL for " .. leaderName .. " at index: " .. i ..". Trying adjacent plot");
+	elseif (pPlot:IsLake()) then
+		print("NOTE: Lake block TSL for " .. leaderName .. " at index: " .. i ..". Trying adjacent plot");
+	-- hard code for Kupe
+	elseif (pPlot:IsWater() and leaderName ~= "LEADER_KUPE") then
+		print("Note: Water tile block for ", leaderName .. " at index: " .. i ..". Trying adjacent plot"); 
+	elseif (pPlot:IsImpassable()) then
+		print("NOTE: Impassible tile blocks TSL for " .. leaderName .. " at index: " .. i ..". Trying adjacent plot");
+
+	-- assign TSL
+	else	
+		pPlayer:SetStartingPlot(pPlot);
+		print("Assigning TSL for", leaderName .. " at index: " .. i);
+		return true;
+	end
+
+	-- o/w try to recursively assign TSL to adjacent plots
+	for _, aPlot in ipairs(self:__GetAdjacentPlots(pPlot)) do
+		if (self:__TryToSetPlayerStartingPlot(pPlayer, leaderName, aPlot)) then break
+		end
+	end
+	return true;
+end
+
 ------------------------------------------------------------------------------
 function AssignTrueStartingPlots:__GetIndex(r, theta)
 	local iX = self.centerX + __Round(r * math.cos(math.rad(theta)));
@@ -78,6 +103,7 @@ function AssignTrueStartingPlots:__GetIndex(r, theta)
 
 	return (iY * width + iX);		-- plot index from polar coord (deg)
 end
+
 ------------------------------------------------------------------------------
 function __Round(n)
 	return math.floor(n + 0.5);
